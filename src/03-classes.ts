1657a75423e0b1304b639f5ae0af49986a2d079b
u4ik <insighteuphoric@gmail.com>
2022-01-05 10:52:06 -0500

    first commit

---
+++ src/03-classes.ts
@@ -0,0 +1,15 @@
+//1
+class Person {
+  //2
+  firstName: string;
+  lastName: string;
+}
+
+//3
+let phil: Person = new Person();
+
+//4
+phil.firstName = "Phil";
+phil.lastName = "Donahue";
+
+phil;
